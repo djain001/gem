43867133efcd593ba98e31b1c7a14619c1bfc435
Diva Jain <divamakesmusic@gmail.com>
2023-06-27 09:35:18 -0400

    Initial Commit

---
+++ gem/gemApp.swift
@@ -0,0 +1,17 @@
+//
+//  gemApp.swift
+//  gem
+//
+//  Created by Scholar on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct gemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
